be4a4814ecc624398b5e4744141dc154a37dd68b
Jg-101 <jghariokwu@gmail.com>
2023-02-16 14:31:48 -0800

    put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - the main is a function
- * 
- * Return: always 0
+ * main - main is a function that contains the code body
+ * Return:  always 0
  */
 
 int main(void)
 {
-          puts("\"Programming is like building a multilingual puzzle");
-	  return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+		return (0);
 }
