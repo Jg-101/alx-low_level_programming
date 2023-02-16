4b86d17340215e1b2a1f47cec4c903370b62427d
Jg-101 <jghariokwu@gmail.com>
2023-02-16 12:23:28 -0800

    put

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - the main is a function
+ * 
+ * Return: always 0
+ */
+
+int main(void)
+{
+          puts("\"Programming is like building a multilingual puzzle");
+	  return (0);
+}
