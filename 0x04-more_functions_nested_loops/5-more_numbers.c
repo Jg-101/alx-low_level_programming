908d63b51663a07dec51323df76f46b84e9c0e44
Jg-101 <jghariokwu@gmail.com>
2023-02-23 16:28:33 -0800

    more

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,12 +9,12 @@ void more_numbers(void)
 {
 	int start = 0, end = 14, i;
 
-	for (i =1; i <= 10; i++)
+	for (i = 1; i <= 10; i++)
 	{
 		while (start <= end)
 		{
 			_putchar(start > 9 ? (start / 10) + '0' : start + '0');
-			
+
 			if (start > 9)
 				_putchar((start % 10) + '0');
 			start++;
