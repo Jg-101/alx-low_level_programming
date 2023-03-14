1f29ba72b434b8d7e918514ef1b26cf563864006
Jg-101 <jghariokwu@gmail.com>
2023-03-14 03:02:46 -0700

    static

---
+++ 0x09-static_libraries/3-strcmp.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strcmp - compare strings
+ * @s1: strings
+ * @s2: strings
+ * Return: 0
+ */
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 && *s2 && *s1 == *s2)
+	{
+		s1++;
+		s2++;
+	}
+	return (*s1 - *s2);
+}
